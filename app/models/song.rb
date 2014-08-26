44d3059975e947b38bf30a36a34591fc66adc8d0
unknown <pedrohrs08@gmail.com>
2014-08-26 18:58:08 -0300

    adding song specs

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,4 +4,8 @@ class Song
   field :name, type: String
   field :artist, type: String
   field :album, type: String
+
+  def number_of_plays
+    Song.where(name: name, artist: artist, album: album).count
+  end
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -5,27 +5,38 @@ describe Song, :type => :model do
     context "class should have correct method " do
       subject { Song.new }
       
-      it { should respond_to :name }
+      it { should respond_to :name   }
       it { should respond_to :artist }
-      it { should respond_to :album }
+      it { should respond_to :album  }
+      it { should respond_to :number_of_plays }
+    end
+    
+    context "#number_of_plays" do
+      before(:each) do
+	    3.times { Song.create!(name: "Pop Song", artist: "Madonna") }
+	  end
+
+	  it "should return the number of occurrences in the db" do
+         record = Song.new(name: "Pop Song", artist: "Madonna")
+         expect(record.number_of_plays).to eq 3
+	  end
     end
     
     context "communication with mongo db" do 
-		before(:each) do
-		   Song.create!(name: "Pop Song", artist: "Madonna")
-		   Song.create!(name: "Folk Song", artist: "Folk Band")
-		end
+	  before(:each) do
+	    Song.create!(name: "Pop Song", artist: "Madonna")
+	    Song.create!(name: "Folk Song", artist: "Folk Band")
+	  end
 
-		it "should create a new song" do
-		  first_song = Song.new(name: "Rock Song", artist: "Rolling Stones")
+	  it "should create a new song" do
+	    first_song = Song.new(name: "Rock Song", artist: "Rolling Stones")
 		  
-		  expect{first_song.save}.to change{Song.count}.by 1
-		end	
+	    expect{first_song.save}.to change{Song.count}.by 1
+	  end	
 
-		it "should retrieve a song" do
-	       first_song = Song.where(name: "Folk Song").first
-
-	       expect(first_song.name).to eq "Folk Song"
-		end
+	  it "should retrieve a song" do
+	    first_song = Song.where(name: "Folk Song").first
+        expect(first_song.name).to eq "Folk Song"
+      end
 	end
 end
